40164821e03a702eb7fe4e06a51c9737778bb51e
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-03-06 20:39:01 +0100

    Producer or Publisher

---
+++ src/main/java/com/example/springrabbitmqone/SpringRabbitMqOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitmqone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitMqOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitMqOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrabbitmqone/SpringRabbitMqOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitmqone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
